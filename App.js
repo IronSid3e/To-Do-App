1561e6b585094cc3e3aecce6310b4000019b20ad
IronSid3e <omertahirustun05@hotmail.com>
2026-02-13 20:32:47 +0300

    initial commit

--- App.js
+++ App.js
@@ -1,123 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import {
-  FlatList,
-  StyleSheet,
-  Text,
-  View,
-  TextInput,
-  TouchableOpacity,
-  Keyboard,
-} from "react-native";
-import React, { useState } from "react";
-import List from "./List";
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
 export default function App() {
-  const [text, setText] = useState("");
-  const [list, setList] = useState([]);
-
-  const toggleTodo = (id) => {
-    const newList = list.map((item) => {
-      if (item.id === id) {
-        return { ...item, completed: !item.completed };
-      }
-      return item;
-    });
-    setList(newList);
-  };
-
-  const addToArray = () => {
-    Keyboard.dismiss();
-    const cleanText = text.trim();
-    if (text && text.trim().length > 0) {
-      const newTodo = {
-        id: Math.random().toString(),
-        title: cleanText,
-        title: text.trim(),
-      };
-      setList([newTodo, ...list]);
-      setText("");
-    }
-  };
-  const removeTodo = (id) => {
-  const newList = list.filter((item) => item.id !== id);
-  setList(newList);
-};
-
   return (
-    <View style={styles.container}>
-      <View style={styles.headerBox}>
-        <Text style={styles.title}>My To Do List</Text>
-      </View>
-
-      <View style={styles.inputArea}>
-        <TextInput
-          style={styles.input}
-          value={text}
-          onChangeText={(val) => setText(val)}
-          placeholder="Yeni görev..."
-        />
-        <TouchableOpacity style={styles.addButton} onPress={addToArray}>
-          <Text style={styles.addButtonText}>+</Text>
-        </TouchableOpacity>
-      </View>
-
-      <FlatList
-        data={list}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <List
-            title={item.title}
-            completed={item.completed}
-            onToggle={() => toggleTodo(item.id)}
-            onRemove={() => removeTodo(item.id)}
-          />
-        )}
-      />
-      <StatusBar style="auto" />
+    <View>
+      <Text>To-do-App</Text>
     </View>
-  );
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#979797",
-    paddingTop: 50,
-  },
-  headerBox: {
-    backgroundColor: "#810000",
-  },
-  title: {
-    fontSize: 30,
-    color: "#fff",
-    padding: 15,
-    textAlign: "center",
-  },
-  inputArea: {
-    flexDirection: "row",
-    padding: 15,
-    alignItems: "center",
-  },
-  input: {
-    flex: 1,
-    borderWidth: 2,
-    borderRadius: 8,
-    borderColor: "#000",
-    padding: 10,
-    backgroundColor: "#fff",
-    marginRight: 10,
-  },
-  addButton: {
-    backgroundColor: "#810000",
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  addButtonText: {
-    color: "#fff",
-    fontSize: 24,
-    fontWeight: "bold",
-  },
-});
+const styles = StyleSheet.create({})
