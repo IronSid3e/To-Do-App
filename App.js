8c5bc80caad9359d6952ed211c093f569a3d90b7
IronSid3e <omertahirustun05@hotmail.com>
2026-02-13 20:58:28 +0300

    add button and function to open modal screen

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import {useState} from "react";
 
 export default function App() {
+  const [ModalIsVisible, setModalIsVisible] = useState(false);
+  const startModel = () => {
+    setModalIsVisible(true);
+  };
   return (
-    <View>
-      <Text>To-do-App</Text>
-    </View>
-  )
+    <>
+      <StatusBar style="light" />
+      <View style={styles.container}>
+        <Button title="Not Ekle" color="#4a0000" onPress={startModel}/>
+      </View>
+    </>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 20
+  },
+});
