248953255537b535e0ca8a3674791f59c25b31d9
IronSid3e <omertahirustun05@hotmail.com>
2026-02-13 22:27:37 +0300

    send props from Modal to app.js

--- App.js
+++ App.js
@@ -1,19 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import { Button, StyleSheet, Text, View } from "react-native";
-import {useState} from "react";
+import { useState } from "react";
 import NewInput from "./components/NewInput";
 
 export default function App() {
   const [modalIsVisible, setModalIsVisible] = useState(false);
-  const startModel = () => {
+  const startModal = () => {
     setModalIsVisible(true);
+  };
+  const endModal = () => {
+    setModalIsVisible(false);
+  };
+  const addElement = (elemetTitle) => {
+    endModal();
   };
   return (
     <>
       <StatusBar style="light" />
       <View style={styles.container}>
-        <Button title="Not Ekle" color="#4a0000" onPress={startModel}/>
-        <NewInput visible = {modalIsVisible}></NewInput>
+        <Button title="Not Ekle" color="#4a0000" onPress={startModal} />
+        <NewInput visible={modalIsVisible} onAddElement={addElement} onCancel={endModal}></NewInput>
       </View>
     </>
   );
@@ -23,6 +29,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: 50,
-    paddingHorizontal: 20
+    paddingHorizontal: 20,
   },
 });
--- components/NewInput.js
+++ components/NewInput.js
@@ -1,31 +1,77 @@
-import { StyleSheet, Text, View, Modal, Image, TextInput, Button } from "react-native";
-import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Modal,
+  Image,
+  TextInput,
+  Button,
+} from "react-native";
+import { useState } from "react";
 
-export default function NewInput({ visible }) {
+export default function NewInput({ visible, onAddElement, onCancel }) {
+  const [enteredText, setEnteredText] = useState("");
+
+  const addNewElement = () => {
+    onAddElement(enteredText);
+    setEnteredText("");
+  };
+
+  const textInputHandler = (text) => {
+    setEnteredText(text);
+  };
   return (
-    <View>
-      <Modal animationType="slide" visible={visible}>
-        <View style= {styles.inputContainer}>
-            <Image style={styles.image} source={require('../assets/logo.png')}/>   
-            <TextInput style= {styles.textInput} placeholder="Yeni Not Ekle.."/>
-          <View style={styles.buttonContainer}>
-            <View style={styles.button}>
-            <Button  title="İptal et" color="red"/>
-            </View>
-            <View style={styles.button}>
-            <Button  title="Ekle" color="blue"/>
-            </View>
+    <Modal animationType="slide" visible={visible}>
+      <View style={styles.inputContainer}>
+        <Image style={styles.image} source={require("../assets/logo.png")} />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Yeni Not Ekle.."
+          value={enteredText}
+          onChangeText={textInputHandler}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="İptal et" color="#ff5c5c" onPress={onCancel} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Ekle" color="#2196F3" onPress={addNewElement} />
           </View>
         </View>
-      </Modal>
-    </View>
+      </View>
+    </Modal>
   );
 }
-
 const styles = StyleSheet.create({
-    inputContainer: {},
-    image:{},
-textInput:{},
-buttonContainer:{},
-button: {}
+  inputContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#34495e",
+    padding: 20,
+  },
+  image: {
+    width: 150,
+    height: 150,
+    borderRadius: 100,
+    marginBottom: 30,
+  },
+  textInput: {
+    width: "100%",
+    borderWidth: 1,
+    borderColor: "#e0e0e0",
+    backgroundColor: "white",
+    color: "#111",
+    borderRadius: 10,
+    padding: 15,
+    marginBottom: 20,
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    width: "80%",
+  },
+  button: {
+    width: "45%",
+  },
 });
